f1f17e14680a19b24fc9d89b26e2ec5807b11902
Erik Forsberg <erik.c.forsberg@gmail.com>
2017-03-29 10:26:14 +0200

    Move UI to box

--- Assets/Scripts/HttpRequest.cs
+++ Assets/Scripts/HttpRequest.cs
@@ -22,9 +22,9 @@ public class HttpRequest : MonoBehaviour {
 
     //Materials
 
-    public Material redMaterial;
-    public Material greenMaterial;
-    public Material blueMaterial;
+    public Material cyanMaterial;
+    public Material magentaMaterial;
+    public Material yellowMaterial;
 
 	// Use this for initialization
 	void Start () {
@@ -103,124 +103,97 @@ public class HttpRequest : MonoBehaviour {
             print("minAge: " + minAge + "\nmaxAge: " + maxAge + "\nmaxLengthOfCustomer: " + maxLengthOfBeingCustomer + "\nminLengthOfCustomer" + minLengthOfBeingCustomer);
 
             int customerCount = 0;
-            // For every product
-            //foreach (JSONObject product in customerData.list)
-            //{
-                /*
-                GameObject customerObjContainer =  new GameObject("product-" + pCount);
-                customerObjContainer.transform.parent = graphContainer.transform.FindChild("Products");
-                customerObjContainer.transform.localRotation = Quaternion.identity;
-                customerObjContainer.transform.localPosition = new Vector3(0, 0, 0); //Set pivot values
-                */
+            // For every customer with that product
+            foreach (JSONObject customer in customerData.list)
+            {
+                // Skips first object with only count data
+                if(customer.HasField("id")){
+                    //GameObject customerObj =  (GameObject) Instantiate(customerElementList[pCount]);
+                    
+                    //Get object by index from object pool, will fail if over 1000 (implement guard for this)
+                    GameObject customerObj = CustomerObjectPool.current.getCustomerObjByIndex(customerCount);
+                    CustomerData customerData = customerObj.GetComponent<CustomerData>();
 
-                // print(product.list[0]["count"]);
-                // print(product.list[1]["product_name"]);
-
-
-                // For every customer with that product
-                foreach (JSONObject customer in customerData.list)
-                {
-                    // Skips first object with only count data
-                    if(customer.HasField("id")){
-                        //GameObject customerObj =  (GameObject) Instantiate(customerElementList[pCount]);
-                        
-                        //Get object by index from object pool, will fail if over 1000 (implement guard for this)
-                        GameObject customerObj = CustomerObjectPool.current.getCustomerObjByIndex(customerCount);
-                        CustomerData customerData = customerObj.GetComponent<CustomerData>();
-
-                        float xPos;
-                        if(customer.HasField("age")){
-                            // (maxAge - minAge) / 2
-                            xPos = (1 / (maxAge - minAge)) * (customer["age"].f - minAge);
-                            if(xPos < 0){
-                                print(customer["age"]);
-                            }
-                            //Set age to script
-                            customerData.age = (int) customer["age"].f;
-                            // xPos = customer["age"].f / 30;
-                        } else{
-                            xPos = Mathf.Abs(Random.insideUnitCircle.x);
-                            print("dunno: " + xPos);
+                    float xPos;
+                    if(customer.HasField("age")){
+                        // (maxAge - minAge) / 2
+                        xPos = (1 / (maxAge - minAge)) * (customer["age"].f - minAge);
+                        if(xPos < 0){
+                            print(customer["age"]);
                         }
-
-                        if(customer.HasField("pivot") && customer["pivot"].HasField("product_id")) {
-                            if(customer["pivot"]["product_id"].f == 1f) {
-                                //set red material
-                                customerObj.transform.GetChild(0).GetComponent<Renderer>().material = redMaterial;
-                            }
-                            else if (customer["pivot"]["product_id"].f == 2f) {
-                                customerObj.transform.GetChild(0).GetComponent<Renderer>().material = blueMaterial;
-                            }
-                            else if (customer["pivot"]["product_id"].f == 6f) {
-                                customerObj.transform.GetChild(0).GetComponent<Renderer>().material = greenMaterial;
-                            }
-                            customerData.productCategoryId = (int) customer["pivot"]["product_id"].f;
-                        }
-                        
-                        float zPos;
-                        if(customer.HasField("months_ago")){
-                            zPos = (1 / (maxLengthOfBeingCustomer - minLengthOfBeingCustomer)) * (customer["months_ago"].f - minLengthOfBeingCustomer);
-                            // zPos = customer["months_ago"].f / 60;
-                            customerData.timeAsCustomerInMonths = (int) customer["months_ago"].f;
-                        } else{
-                            zPos = Random.insideUnitCircle.y;
-                        }
-
-                        customerData.npsScore = (int)customer["nps_scores"].list[0]["score"].f;
-
-                        // för användning av Antons graf
-                        // customerObj.transform.position = customerObjContainer.transform.position + new Vector3(
-                        //     (customer["nps_scores"].list[0]["score"].f / 50f) * 5.77f, 
-                        //     Random.Range(0f, 8.5f/5f),
-                        //     0f);
-
-                        //customerObj.transform.parent = graphContainer.transform.Find("Products").transform;
-
-                        customerObj.transform.rotation = Quaternion.identity;
-
-                        customerObj.transform.localPosition = new Vector3(
-                            xPos * 5,
-                            customer["nps_scores"].list[0]["score"].f / 2,
-                            zPos * 5);
-
-
-                        //Sets saturation depending on age.
-
-                        /*
-                        Color emissiveColors = customerObj.transform.GetChild(0).GetComponent<Renderer>().material.GetColor("_EmissionColor");
-                        print(emissiveColors);
-                        emissiveColors.r = emissiveColors.r * (0.5f / (0.5f + xPos));
-                        emissiveColors.g = emissiveColors.g * (0.5f / (0.5f + xPos));
-                        emissiveColors.b = emissiveColors.b * (0.5f / (0.5f + xPos));
-                        emissiveColors.a = emissiveColors.a * (0.1f / (0.1f + xPos));
-                        print(emissiveColors);
-                        customerObj.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_EmissionColor", emissiveColors);
-                        
-                        Color colors = customerObj.transform.GetChild(0).GetComponent<Renderer>().material.GetColor("_Color");
-                        print(colors);
-                        colors.r = colors.r * (0.1f / (0.1f + xPos));
-                        colors.g = colors.g * (0.1f / (0.1f + xPos));
-                        colors.b = colors.b * (0.1f / (0.1f + xPos));
-                        // colors.a = colors.a * (0.1f / (0.1f + xPos));
-                        print(colors);
-                        customerObj.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", colors);
-                        */
-
-
-                        // For every key, if ever needed
-                        // foreach (var key in customer.keys)
-                        // {
-                        //     print(key + ": " + customer[key]);
-                            
-                        // }
+                        //Set age to script
+                        customerData.age = (int) customer["age"].f;
+                        // xPos = customer["age"].f / 30;
+                    } else{
+                        xPos = Mathf.Abs(Random.insideUnitCircle.x);
+                        print("dunno: " + xPos);
                     }
-                    customerCount++;
+
+                    if(customer.HasField("pivot") && customer["pivot"].HasField("product_id")) {
+                        if(customer["pivot"]["product_id"].f == 1f) {
+                            //set red material
+                            customerObj.transform.GetChild(0).GetComponent<Renderer>().material = cyanMaterial;
+                        }
+                        else if (customer["pivot"]["product_id"].f == 2f) {
+                            customerObj.transform.GetChild(0).GetComponent<Renderer>().material = magentaMaterial;
+                        }
+                        else if (customer["pivot"]["product_id"].f == 6f) {
+                            customerObj.transform.GetChild(0).GetComponent<Renderer>().material = yellowMaterial;
+                        }
+                        customerData.productCategoryId = (int) customer["pivot"]["product_id"].f;
+                    }
+                    
+                    float zPos;
+                    if(customer.HasField("months_ago")){
+                        zPos = (1 / (maxLengthOfBeingCustomer - minLengthOfBeingCustomer)) * (customer["months_ago"].f - minLengthOfBeingCustomer);
+                        // zPos = customer["months_ago"].f / 60;
+                        customerData.timeAsCustomerInMonths = (int) customer["months_ago"].f;
+                    } else{
+                        zPos = Random.insideUnitCircle.y;
+                    }
+
+                    customerData.npsScore = (int)customer["nps_scores"].list[0]["score"].f;
+
+
+                    customerObj.transform.rotation = Quaternion.identity;
+                    customerObj.transform.localPosition = new Vector3(
+                        xPos * 5,
+                        customer["nps_scores"].list[0]["score"].f / 2,
+                        zPos * 5);
+
+
+                    //Sets saturation depending on age.
+
+                    /*
+                    Color emissiveColors = customerObj.transform.GetChild(0).GetComponent<Renderer>().material.GetColor("_EmissionColor");
+                    print(emissiveColors);
+                    emissiveColors.r = emissiveColors.r * (0.5f / (0.5f + xPos));
+                    emissiveColors.g = emissiveColors.g * (0.5f / (0.5f + xPos));
+                    emissiveColors.b = emissiveColors.b * (0.5f / (0.5f + xPos));
+                    emissiveColors.a = emissiveColors.a * (0.1f / (0.1f + xPos));
+                    print(emissiveColors);
+                    customerObj.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_EmissionColor", emissiveColors);
+                    
+                    Color colors = customerObj.transform.GetChild(0).GetComponent<Renderer>().material.GetColor("_Color");
+                    print(colors);
+                    colors.r = colors.r * (0.1f / (0.1f + xPos));
+                    colors.g = colors.g * (0.1f / (0.1f + xPos));
+                    colors.b = colors.b * (0.1f / (0.1f + xPos));
+                    // colors.a = colors.a * (0.1f / (0.1f + xPos));
+                    print(colors);
+                    customerObj.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", colors);
+                    */
+
+
+                    // For every key, if ever needed
+                    // foreach (var key in customer.keys)
+                    // {
+                    //     print(key + ": " + customer[key]);
+                        
+                    // }
                 }
-
-                // customerObjContainer.transform.eulerAngles = new Vector3(0, pCount * 72, 0);
-                //pCount++;
-            //}
-
+                customerCount++;
+            }
 		}
     }
 
